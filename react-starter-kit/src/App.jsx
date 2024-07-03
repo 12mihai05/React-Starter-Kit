5086a1a0812d326040bbe8be296ef473a0ddae05
Pasaroiu Mihai <pasaroiumihai@yahoo.com>
2024-07-03 15:41:16 +0300

    first commit

---
+++ react-starter-kit/src/App.jsx
@@ -0,0 +1,15 @@
+import ToDoList from './ToDoList/ToDoList.jsx';
+import './ToDoList/ToDoList.css'
+
+function App() {
+
+
+
+  return(
+    <>
+      <ToDoList/>
+    </>
+  );
+} 
+
+export default App
